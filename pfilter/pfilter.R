47e4c914b2f3d00b27214689d8b90c444f62e47c
Aaron A. King <kingaa@umich.edu>
2015-07-19 23:43:32 -0400

    - beta -> Beta to avoid warnings

--- pfilter/pfilter.R
+++ pfilter/pfilter.R
@@ -23,7 +23,7 @@ url <- paste0(base_url,"data/bsflu_data.txt")
 bsflu <- read.table(url)
 
 sir_step <- Csnippet("
-  double dN_SI = rbinom(S,1-exp(-beta*I/N*dt));
+  double dN_SI = rbinom(S,1-exp(-Beta*I/N*dt));
   double dN_IR = rbinom(I,1-exp(-gamma*dt));
   S -= dN_SI;
   I += dN_SI - dN_IR;
@@ -45,10 +45,10 @@ pomp(bsflu,times="day",t0=0,
      rprocess=euler.sim(sir_step,delta.t=1/5),
      initializer=sir_init,rmeasure=rmeas,dmeasure=dmeas,
      zeronames="H",statenames=c("H","S","I","R"),
-     paramnames=c("beta","gamma","rho","N")) -> sir
+     paramnames=c("Beta","gamma","rho","N")) -> sir
 
 ## ----bbs-mc-like-2,results='markup'--------------------------------------
-simulate(sir,params=c(beta=2,gamma=1,rho=0.5,N=2600),
+simulate(sir,params=c(Beta=2,gamma=1,rho=0.5,N=2600),
          nsim=10000,states=TRUE) -> x
 matplot(time(sir),t(x["H",1:50,]),type='l',lty=1,
         xlab="time",ylab="H",bty='l',col='blue')
@@ -56,32 +56,32 @@ lines(time(sir),obs(sir,"B"),lwd=2,col='black')
 
 ## ----bbs-mc-like-3,results='markup',cache=T------------------------------
 ell <- dmeasure(sir,y=obs(sir),x=x,times=time(sir),log=TRUE,
-                params=c(beta=2,gamma=1,rho=0.5,N=2600))
+                params=c(Beta=2,gamma=1,rho=0.5,N=2600))
 dim(ell)
 
 ## ----bbs-mc-like-4,results='markup'--------------------------------------
 ell <- apply(ell,1,sum); summary(exp(ell)); logmeanexp(ell,se=TRUE)
 
 ## ----sir-sim1------------------------------------------------------------
-sims <- simulate(sir,params=c(beta=2,gamma=1,rho=0.8,N=2600),nsim=20,
+sims <- simulate(sir,params=c(Beta=2,gamma=1,rho=0.8,N=2600),nsim=20,
                  as.data.frame=TRUE,include.data=TRUE)
 
 ggplot(sims,mapping=aes(x=time,y=B,group=sim,color=sim=="data"))+
   geom_line()+guides(color=FALSE)
 
 ## ----sir-pfilter-1,results='markup',cache=T------------------------------
-pf <- pfilter(sir,Np=5000,params=c(beta=2,gamma=1,rho=0.8,N=2600))
+pf <- pfilter(sir,Np=5000,params=c(Beta=2,gamma=1,rho=0.8,N=2600))
 logLik(pf)
 
 ## ----sir-pfilter-2,results='markup',cache=T------------------------------
-pf <- replicate(10,pfilter(sir,Np=5000,params=c(beta=2,gamma=1,rho=0.8,N=2600)))
+pf <- replicate(10,pfilter(sir,Np=5000,params=c(Beta=2,gamma=1,rho=0.8,N=2600)))
 ll <- sapply(pf,logLik); ll
 logmeanexp(ll,se=TRUE)
 
 ## ----sir-like-slice,cache=TRUE,results='hide'----------------------------
 sliceDesign(
-  c(beta=2,gamma=1,rho=0.8,N=2600),
-  beta=rep(seq(from=0.5,to=4,length=40),each=3),
+  c(Beta=2,gamma=1,rho=0.8,N=2600),
+  Beta=rep(seq(from=0.5,to=4,length=40),each=3),
   gamma=rep(seq(from=0.5,to=2,length=40),each=3)) -> p
 
 require(foreach)
@@ -100,14 +100,14 @@ foreach (theta=iter(p,"row"),.combine=rbind,
  } -> p
 
 ## ----sir-like-slice-plot,cache=F,results="hide"--------------------------
-foreach (v=c("beta","gamma")) %do% 
+foreach (v=c("Beta","gamma")) %do% 
 {
   x <- subset(p,slice==v)
   plot(x[[v]],x$loglik,xlab=v,ylab="loglik")
 }
 
 ## ----sir-grid1-----------------------------------------------------------
-expand.grid(beta=seq(from=1,to=4,length=50),
+expand.grid(Beta=seq(from=1,to=4,length=50),
             gamma=seq(from=0.7,to=3,length=50),
             rho=0.8,
             N=2600) -> p
@@ -123,7 +123,7 @@ foreach (theta=iter(p,"row"),.combine=rbind,
 
 ## ----sir-grid1-plot------------------------------------------------------
 pp <- mutate(p,loglik=ifelse(loglik>max(loglik)-100,loglik,NA))
-ggplot(data=pp,mapping=aes(x=beta,y=gamma,z=loglik,fill=loglik))+
+ggplot(data=pp,mapping=aes(x=Beta,y=gamma,z=loglik,fill=loglik))+
   geom_tile(color=NA)+
   geom_contour(color='black',binwidth=3)+
   scale_fill_gradient()+
@@ -131,23 +131,23 @@ ggplot(data=pp,mapping=aes(x=beta,y=gamma,z=loglik,fill=loglik))+
 
 ## ----sir-partrans--------------------------------------------------------
 toEst <- Csnippet("
- Tbeta = log(beta);
+ TBeta = log(Beta);
  Tgamma = log(gamma);
  Trho = logit(rho);
 ")
 
 fromEst <- Csnippet("
- Tbeta = exp(beta);
+ TBeta = exp(Beta);
  Tgamma = exp(gamma);
  Trho = expit(rho);
 ")
 
 pomp(sir,toEstimationScale=toEst,
      fromEstimationScale=fromEst,
-     paramnames=c("beta","gamma","rho")) -> sir
+     paramnames=c("Beta","gamma","rho")) -> sir
 
 ## ----sir-ref-params------------------------------------------------------
-coef(sir) <- c(beta=2,gamma=1,rho=0.8,N=2600)
+coef(sir) <- c(Beta=2,gamma=1,rho=0.8,N=2600)
 
 ## ----sir-like-optim-1,echo=T,eval=T,results='markup',cache=T-------------
 neg.ll <- function (par, est, ...) {
@@ -169,15 +169,15 @@ neg.ll <- function (par, est, ...) {
 ## use Nelder-Mead with fixed RNG seed
 fit <- optim(
   par=c(log(1), log(2), log(0.8)),
-  est=c("gamma","beta","rho"),
+  est=c("gamma","Beta","rho"),
   fn=neg.ll,
   method="Nelder-Mead",
   control=list(maxit=400,trace=0)
 )
 
 mle <- sir
-coef(mle,c("gamma","beta","rho"),transform=TRUE) <- fit$par
-coef(mle,c("gamma","beta","rho")) ## point estimate
+coef(mle,c("gamma","Beta","rho"),transform=TRUE) <- fit$par
+coef(mle,c("gamma","Beta","rho")) ## point estimate
 
 fit$val
 
