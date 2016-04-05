bdd6bdfb1e751a4e03ef1f6957d072142e429d4c
Aaron A. King <kingaa@umich.edu>
2016-04-04 22:35:51 -0400

    - unroll the 'mif' document

--- mif/mif.R
+++ mif/mif.R
@@ -1,8 +1,7 @@
 ## ----prelims,include=FALSE,purl=TRUE,cache=FALSE-------------------------
-options(
-  keep.source=TRUE,
-  encoding="UTF-8"
-)
+library(pomp)
+options(cores=20,stringsAsFactors=FALSE)
+stopifnot(packageVersion("pomp")>="1.4")
 
 ## ----load_bbs------------------------------------------------------------
 bsflu_data <- read.table("http://kingaa.github.io/sbied/data/bsflu_data.txt")
@@ -55,8 +54,7 @@ bsflu_initializer <- "
 "
 
 ## ----pomp_bsflu----------------------------------------------------------
-require(pomp)
-stopifnot(packageVersion("pomp")>="1.2.2")
+library(pomp)
 bsflu <- pomp(
   data=bsflu_data,
   times="day",
@@ -81,8 +79,8 @@ params <- c(Beta=0.005,mu_I=2,rho=0.9,mu_R1=1/3,mu_R2=1/2)
 ## ----init_sim------------------------------------------------------------
 y <- simulate(bsflu,params=params,nsim=10,as.data.frame=TRUE)
 
-require(ggplot2)
-require(reshape2)
+library(ggplot2)
+library(reshape2)
 
 ggplot(data=melt(subset(y,select=c(time,B,C,sim)),
                  id=c("sim","time")),
@@ -95,50 +93,37 @@ ggplot(data=melt(subset(y,select=c(time,B,C,sim)),
 pf <- pfilter(bsflu,params=params,Np=1000)
 plot(pf)
 
-## ----run_level-----------------------------------------------------------
-run_level <- 3
-switch(run_level,
-       {bsflu_Np=100; bsflu_Nmif=10; bsflu_Neval=10; bsflu_Nglobal=10; bsflu_Nlocal=10}, 
-       {bsflu_Np=20000; bsflu_Nmif=100; bsflu_Neval=10; bsflu_Nglobal=10; bsflu_Nlocal=10}, 
-       {bsflu_Np=60000; bsflu_Nmif=300; bsflu_Neval=10; bsflu_Nglobal=100; bsflu_Nlocal=20}
-)
-
 ## ----fixed_params--------------------------------------------------------
-fixed_params <- with(bsflu_data,c(mu_R1=1/(sum(B)/512),mu_R2=1/(sum(C)/512)))
-est_params <- params[c("Beta","mu_I","rho")]
+(fixed_params <- with(bsflu_data,c(mu_R1=1/(sum(B)/512),mu_R2=1/(sum(C)/512))))
 
 ## ----parallel-setup,cache=FALSE------------------------------------------
-require(foreach)
-require(doParallel)
+library(foreach)
+library(doParallel)
 
-cores <- 20
-registerDoParallel(cores)
+registerDoParallel()
 
 set.seed(2036049659,kind="L'Ecuyer")
-mcopts <- list(set.seed=TRUE)
 
-## ----pf------------------------------------------------------------------
-stew(file="pf-1.rda",{
+## ----pf,echo=F,eval=T----------------------------------------------------
+stew(file="pf.rda",{
   
   t_pf <- system.time(
-    pf <- foreach(i=1:20,.packages='pomp',
+    pf <- foreach(i=1:10,.packages='pomp',
                   .options.multicore=list(set.seed=TRUE)) %dopar% 
       try(
-        pfilter(bsflu,params=c(est_params,fixed_params),Np=1000)
+        pfilter(bsflu,params=c(Beta=0.01,mu_I=2,rho=0.9,fixed_params),Np=10000)
       )
   )
-  
 },seed=625904618,kind="L'Ecuyer")
 
 (L_pf <- logmeanexp(sapply(pf,logLik),se=TRUE))
 
 ## ----init_csv------------------------------------------------------------
-results <- as.data.frame(as.list(c(est_params,fixed_params,loglik=L_pf[1],loglik.se=L_pf[2])))
-write.csv(results,file="mif_bsflu_params.csv",row.names=FALSE)
+results <- as.data.frame(as.list(c(coef(pf[[1]]),loglik=L_pf[1],loglik=L_pf[2])))
+write.csv(results,file="bsflu_params.csv",row.names=FALSE)
 
 ## ----box_search_local----------------------------------------------------
-stew(file="local_search-1.rda",{
-  
+stew(file="local_search.rda",{
   t_local <- system.time({
     mifs_local <- foreach(i=1:20,
                           .packages='pomp',
@@ -147,39 +132,47 @@ stew(file="local_search-1.rda",{
       try(
         mif2(
           bsflu,
-          start=c(fixed_params,est_params),
-          Np=20000,
-          Nmif=100,
+          start=c(Beta=0.01,mu_I=2,rho=0.9,fixed_params),
+          Np=2000,
+          Nmif=50,
           cooling.type="geometric",
           cooling.fraction.50=0.5,
           transform=TRUE,
           rw.sd=rw.sd(Beta=0.02,mu_I=0.02,rho=0.02)
         )
       )
-  })  
+  })
 },seed=482947940,kind="L'Ecuyer")
 
+ggplot(data=melt(conv.rec(mifs_local)),
+       aes(x=iteration,y=value,group=L1,color=factor(L1)))+
+    geom_line()+
+    guides(color=FALSE)+
+    facet_wrap(~variable,scales="free_y")+
+    theme_bw()
 
 ## ----lik_local_eval------------------------------------------------------
-stew(file="lik_local-1.rda",{
-  
+stew(file="lik_local.rda",{
   t_local_eval <- system.time({
-    liks_local <- foreach(mf=mifs_local,
-                          .packages='pomp',
-                          .combine=rbind) %dopar% 
+    results_local <- foreach(mf=mifs_local,
+                             .packages='pomp',
+                             .combine=rbind,
+                             .options.multicore=list(set.seed=TRUE)) %dopar% 
       try({
         evals <- replicate(10, logLik(pfilter(mf,Np=20000)))
         ll <- logmeanexp(evals,se=TRUE)
-        c(coef(mf),loglik=ll)
+        c(coef(mf),loglik=ll[1],loglik=ll[2])
       })
   })
 },seed=900242057,kind="L'Ecuyer")
-
-results_local <- data.frame(logLik=liks_local[,1],logLik_se=liks_local[,2],t(sapply(mifs_local,coef)))
-summary(results_local$logLik,digits=5)
+results_local <- as.data.frame(results_local)
 
 ## ----pairs_local---------------------------------------------------------
-pairs(~logLik+Beta+mu_I+rho,data=subset(results_local,logLik>max(logLik)-50))
+pairs(~loglik+Beta+mu_I+rho,data=results_local)
+
+## ----local_database------------------------------------------------------
+results <- rbind(results,results_local[names(results)])
+write.csv(results,file="bsflu_params.csv",row.names=FALSE)
 
 ## ----box-----------------------------------------------------------------
 bsflu_box <- rbind(
@@ -189,35 +182,38 @@ bsflu_box <- rbind(
 )
 
 ## ----box_eval------------------------------------------------------------
-stew(file=sprintf("box_eval-%d.rda",run_level),{
-  
-  t_global <- system.time({
-    mifs_global <- foreach(i=1:bsflu_Nglobal,.packages='pomp', .combine=c, .options.multicore=mcopts) %dopar%  mif2(
-      mifs_local[[1]],
-      start=c(apply(bsflu_box,1,function(x)runif(1,x[1],x[2])),fixed_params)
-    )
-  })
+stew(file="box_eval.rda",{
+    t_global <- system.time({
+        mifs_global <- foreach(i=1:300, 
+                               .packages='pomp', 
+                               .combine=c,
+                               .options.multicore=list(set.seed=TRUE)
+                               ) %dopar%  
+        mif2(
+            mifs_local[[1]],
+            start=c(apply(bsflu_box,1,function(x)runif(1,x[1],x[2])),fixed_params)
+        )
+    })
 },seed=1270401374,kind="L'Ecuyer")
 
-## ----lik_global_eval-----------------------------------------------------
-stew(file=sprintf("lik_global_eval-%d.rda",run_level),{
-  
-  t_global_eval <- system.time({
-    liks_global <- foreach(i=1:bsflu_Nglobal,.packages='pomp',.combine=rbind, .options.multicore=mcopts) %dopar% {
-      evals <- replicate(bsflu_Neval, logLik(pfilter(bsflu,params=coef(mifs_global[[i]]),Np=bsflu_Np)))
-      logmeanexp(evals, se=TRUE)
-    }
-  })
+stew(file="lik_global_eval.rda",{
+    t_global_eval <- system.time({
+        results_global <- foreach(mf=mifs_global,
+                                  .packages='pomp',
+                                  .combine=rbind,
+                                  .options.multicore=list(set.seed=TRUE)) %dopar%
+        {
+            evals <- replicate(10,logLik(pfilter(mf,Np=20000)))
+            ll <- logmeanexp(evals,se=TRUE)
+            c(coef(mf),loglik=ll[1],loglik=ll[2])
+        }
+    })
 },seed=442141592,kind="L'Ecuyer")
 
-results_global <- data.frame(logLik=liks_global[,1],logLik_se=liks_global[,2],t(sapply(mifs_global,coef)))
-summary(results_global$logLik,digits=5)
-
-## ----save_params,eval=FALSE----------------------------------------------
-## if (run_level>1)
-##   write.table(rbind(results_local,results_global),
-##               file="mif_bsflu_params.csv",append=TRUE,col.names=FALSE,row.names=FALSE)
+results_global <- as.data.frame(results_global)
+results <- rbind(results,results_global[names(results)])
+write.csv(results,file="bsflu_params.csv",row.names=FALSE)
 
 ## ----pairs_global--------------------------------------------------------
-pairs(~logLik+Beta+mu_I+rho,data=subset(results_global,logLik>max(logLik)-250))
+pairs(~loglik+Beta+mu_I+rho,data=results_global)
 
