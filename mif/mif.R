b52014a69506bfd372602dd97e5bfb39cd808d81
Aaron A. King <kingaa@umich.edu>
2016-04-05 13:13:22 -0400

    - finish unrolling the mif document

--- mif/mif.R
+++ mif/mif.R
@@ -10,9 +10,6 @@ bsflu_data <- read.table("http://kingaa.github.io/sbied/data/bsflu_data.txt")
 bsflu_statenames <- c("S","I","R1","R2")
 bsflu_paramnames <- c("Beta","mu_I","rho","mu_R1","mu_R2")
 
-## ----bsflu_obsnames------------------------------------------------------
-colnames(bsflu_data)[1:2]
-
 ## ----csnippets_bsflu-----------------------------------------------------
 bsflu_dmeasure <- "
   lik = dpois(B,rho*R1+1e-6,give_log);
@@ -71,7 +68,6 @@ bsflu <- pomp(
   paramnames=bsflu_paramnames,
   initializer=Csnippet(bsflu_initializer)
 )
-plot(bsflu)
 
 ## ----start_params--------------------------------------------------------
 params <- c(Beta=0.005,mu_I=2,rho=0.9,mu_R1=1/3,mu_R2=1/2)
@@ -79,19 +75,8 @@ params <- c(Beta=0.005,mu_I=2,rho=0.9,mu_R1=1/3,mu_R2=1/2)
 ## ----init_sim------------------------------------------------------------
 y <- simulate(bsflu,params=params,nsim=10,as.data.frame=TRUE)
 
-library(ggplot2)
-library(reshape2)
-
-ggplot(data=melt(subset(y,select=c(time,B,C,sim)),
-                 id=c("sim","time")),
-       mapping=aes(x=time,y=value,group=sim))+
-  geom_line()+
-  facet_grid(variable~.)+
-  theme_classic()
-
 ## ----init_pfilter--------------------------------------------------------
 pf <- pfilter(bsflu,params=params,Np=1000)
-plot(pf)
 
 ## ----fixed_params--------------------------------------------------------
 (fixed_params <- with(bsflu_data,c(mu_R1=1/(sum(B)/512),mu_R2=1/(sum(C)/512))))
@@ -104,7 +89,7 @@ registerDoParallel()
 
 set.seed(2036049659,kind="L'Ecuyer")
 
-## ----pf,echo=F,eval=T----------------------------------------------------
+## ----pf------------------------------------------------------------------
 stew(file="pf.rda",{
   t_pf <- system.time(
     pf <- foreach(i=1:10,.packages='pomp',
@@ -123,7 +108,7 @@ write.csv(results,file="bsflu_params.csv",row.names=FALSE)
 
 ## ----box_search_local----------------------------------------------------
 stew(file="box_search_local.rda",{
-  t_local <- system.time({
+  t_local_mif <- system.time({
     mifs_local <- foreach(i=1:20,
                           .packages='pomp',
                           .combine=c, 
@@ -143,56 +128,46 @@ stew(file="box_search_local.rda",{
   })
 },seed=482947940,kind="L'Ecuyer")
 
-ggplot(data=melt(conv.rec(mifs_local)),
-       aes(x=iteration,y=value,group=L1,color=factor(L1)))+
-  geom_line()+
-  guides(color=FALSE)+
-  facet_wrap(~variable,scales="free_y")+
-  theme_bw()
-
 ## ----lik_local-----------------------------------------------------------
 stew(file="lik_local.rda",{
-  t_local <- system.time({
+  t_local_eval <- system.time({
     results_local <- foreach(mf=mifs_local,
                              .packages='pomp',
                              .combine=rbind,
                              .options.multicore=list(set.seed=TRUE)) %dopar% 
-    {
-        evals <- replicate(10, logLik(pfilter(mf,Np=20000)))
-        ll <- logmeanexp(evals,se=TRUE)
-        c(coef(mf),loglik=ll[1],loglik=ll[2])
-    }
+                             {
+                               evals <- replicate(10, logLik(pfilter(mf,Np=20000)))
+                               ll <- logmeanexp(evals,se=TRUE)
+                               c(coef(mf),loglik=ll[1],loglik=ll[2])
+                             }
   })
 },seed=900242057,kind="L'Ecuyer")
 results_local <- as.data.frame(results_local)
-
-## ----pairs_local---------------------------------------------------------
-pairs(~loglik+Beta+mu_I+rho,data=results_local)
 
 ## ----local_database------------------------------------------------------
 results <- rbind(results,results_local[names(results)])
 write.csv(results,file="bsflu_params.csv",row.names=FALSE)
 
-## ----box-----------------------------------------------------------------
+## ----box_global----------------------------------------------------------
 bsflu_box <- rbind(
   Beta=c(0.001,0.01),
-  mu_I=c(0.5,2),
+  mu_I=c(0.5,3),
   rho = c(0.5,1)
 )
 
 ## ----box_search_global---------------------------------------------------
 stew(file="box_search_global.rda",{
   t_global <- system.time({
-    results_global <- foreach(i=1:300, 
+    guesses <- as.data.frame(apply(bsflu_box,1,function(x)runif(300,x[1],x[2])))
+    results_global <- foreach(guess=iter(guesses,"row"), 
                               .packages='pomp', 
                               .combine=rbind,
                               .options.multicore=list(set.seed=TRUE)
     ) %dopar% {
-      guess <- apply(bsflu_box,1,function(x)runif(1,x[1],x[2]))
-      mf <- mif2(mifs_local[[1]],start=c(guess,fixed_params),
+      mf <- mif2(mifs_local[[1]],start=c(unlist(guess),fixed_params),
                  cooling.type='geometric')
       mf <- mif2(mf,Nmif=100)
-      ll <- replicate(10,logLik(pfilter(mf,Np=20000)))
+      ll <- replicate(10,logLik(pfilter(mf,Np=100000)))
       ll <- logmeanexp(ll,se=TRUE)
       c(coef(mf),loglik=ll[1],loglik=ll[2])
     }
@@ -201,7 +176,4 @@ stew(file="box_search_global.rda",{
 results_global <- as.data.frame(results_global)
 results <- rbind(results,results_global[names(results)])
 write.csv(results,file="bsflu_params.csv",row.names=FALSE)
-
-## ----pairs_global--------------------------------------------------------
-pairs(~loglik+Beta+mu_I+rho,data=results_global)
 
