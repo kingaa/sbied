9ff3b8a26f16f1f6862530e2d61d1a14e46999f3
Aaron A. King <kingaa@umich.edu>
2016-07-14 07:38:50 -0400

    more miffing

--- seir3/seir3.R
+++ seir3/seir3.R
@@ -41,6 +41,7 @@ set.seed(1221234211)
 #' Implement it in **pomp** using a compartmental model like that diagrammed below.
 #' You will have to give some thought to just how to model the relationship between the data ($B$ and $C$) and the state variables.
 #' 
+#' 
 ## ----seir3_model---------------------------------------------------------
 read.table("http://kingaa.github.io/sbied/stochsim/bsflu_data.txt") -> bsflu
 
@@ -137,8 +138,200 @@ lls %>%
   labs(fill="nfilt")
 
 #' 
+#' ## Maximizing the likelihood using IF2
+#' 
+#' Fixed parameters:
+#' 
+## ----fixed_params--------------------------------------------------------
+(fixed_params <- with(bsflu,c(mu_R1=1/(sum(B)/512),mu_R2=1/(sum(C)/512))))
+
+#' 
+#' We will estimate $\beta$, $\mu_E$, $\mu_I$, and $\rho$.
+#' 
+#' 
+#' ### Local search
+#' 
+## ----box_search_local----------------------------------------------------
+pomp(flu,
+     toEstimationScale=Csnippet("
+        Trho = logit(rho);
+        TBeta = log(Beta);
+        Tmu_E = log(mu_E);
+        Tmu_I = log(mu_I);"),
+      fromEstimationScale=Csnippet("
+        Trho =expit(rho);
+        TBeta =exp(Beta);
+        Tmu_E =exp(mu_E);
+        Tmu_I =exp(mu_I);"),
+     paramnames=c("rho","Beta","mu_E","mu_I")
+) -> flu
+
+registerDoParallel()
+
+bake(file="box_search_local.rds",
+     seed=958561634,kind="L'Ecuyer",{
+  foreach(i=1:20,
+          .packages='pomp',
+          .combine=c, 
+          .options.multicore=list(set.seed=TRUE),
+          .export=c("flu","fixed_params")
+  ) %dopar%  
+  {
+    mif2(
+      flu,
+      start=c(Beta=6,mu_E=0.5,mu_I=2,rho=0.9,fixed_params),
+      Np=2000,
+      Nmif=50,
+      cooling.type="geometric",
+      cooling.fraction.50=0.5,
+      transform=TRUE,
+      rw.sd=rw.sd(Beta=0.02,mu_E=0.02,mu_I=0.02,rho=0.02)
+    )
+  }
+}) -> mifs_local
+
+#' 
+#' We obtain some diagnostic plots with the `plot` command applied to `mifs_local`.
+#' Here is a way to get a prettier version:
+#' 
+#' 
+#' 
+## ----lik_local-----------------------------------------------------------
+bake(file="lik_local.rds",
+     seed=597255167,kind="L'Ecuyer",
+     {
+       foreach(mf=mifs_local,
+               .packages='pomp',
+               .combine=rbind,
+               .options.multicore=list(set.seed=TRUE)
+       ) %dopar% 
+       {
+         evals <- replicate(10, logLik(pfilter(mf,Np=20000)))
+         ll <- logmeanexp(evals,se=TRUE)
+         c(coef(mf),loglik=ll[1],loglik=ll[2])
+       }
+     }) -> results
+results <- as.data.frame(results)
+write.csv(results,file="seir3_params.csv",row.names=FALSE)
+
+#' 
+#' ### Global search
+#' 
+## ----box_global----------------------------------------------------------
+params_box <- rbind(
+  Beta=c(1,8),
+  mu_E=c(0.5,10),
+  mu_I=c(0.5,10),
+  rho = c(0.5,1)
+)
+
+#' 
+## ----box_search_global---------------------------------------------------
+bake(file="box_search_global.rds",
+     seed=173139436,kind="L'Ecuyer",
+     {
+       mf1 <- mifs_local[[1]]
+       guesses <- as.data.frame(apply(params_box,1,
+                                      function(x)runif(300,x[1],x[2])))
+       foreach(guess=iter(guesses,"row"), 
+               .packages='pomp', 
+               .combine=rbind,
+               .options.multicore=list(set.seed=TRUE),
+               .export=c("mf1","fixed_params")
+       ) %dopar% 
+       {
+         mf <- mif2(mf1,start=c(unlist(guess),fixed_params))
+         mf <- mif2(mf,Nmif=100)
+         ll <- replicate(10,logLik(pfilter(mf,Np=100000)))
+         ll <- logmeanexp(ll,se=TRUE)
+         c(coef(mf),loglik=ll[1],loglik=ll[2])
+       }
+     }) -> results_global
+results_global <- as.data.frame(results_global)
+results <- rbind(results,results_global[names(results)])
+write.csv(results,file="seir3_params.csv",row.names=FALSE)
+
+#' 
+## ----fig.height=8,fig.width=8--------------------------------------------
+pairs(~loglik+Beta+mu_E+mu_I+rho, data=subset(results,loglik>max(loglik)-50),
+      pch=16)
+
+#' 
+#' ### Global search 2
+#' 
+## ----box_global2---------------------------------------------------------
+library(plyr)
+library(reshape2)
+library(magrittr)
+
+results %>%
+    subset(loglik>max(loglik)-50) %>%
+    melt(id=NULL) %>%
+    daply(~variable,function(x)range(x$value)) -> params_box
+
+#' 
+## ----box_search_global2--------------------------------------------------
+bake(file="box_search_global2.rds",
+     seed=727349120,kind="L'Ecuyer",
+     {
+       mf1 <- mifs_local[[1]]
+       guesses <- as.data.frame(apply(params_box,1,
+                                      function(x)runif(300,x[1],x[2])))
+       foreach(guess=iter(guesses,"row"), 
+               .packages='pomp', 
+               .combine=rbind,
+               .options.multicore=list(set.seed=TRUE),
+               .export=c("mf1","fixed_params")
+       ) %dopar% 
+       {
+         mf <- mif2(mf1,start=unlist(guess))
+         mf <- mif2(mf,Nmif=100)
+         ll <- replicate(10,logLik(pfilter(mf,Np=100000)))
+         ll <- logmeanexp(ll,se=TRUE)
+         c(coef(mf),loglik=ll[1],loglik=ll[2])
+       }
+     }) -> results_global
+results_global <- as.data.frame(results_global)
+results <- rbind(results,results_global[names(results)])
+write.csv(results,file="seir3_params.csv",row.names=FALSE)
+
+#' 
 ## ------------------------------------------------------------------------
-fixed_params <- with(bsflu,c(mu_R1=1/(sum(B)/512),mu_R2=1/(sum(C)/512)))
+pairs(~loglik+Beta+mu_E+mu_I+rho, data=subset(results,loglik>max(loglik)-10),pch=16)
+
+#' 
+## ----profile-------------------------------------------------------------
+bake(file="profile1.rds",
+     seed=1994223858,kind="L'Ecuyer",
+     {
+         guesses <- as.data.frame(apply(params_box,1,
+                                        function(x)runif(300,x[1],x[2])))
+         foreach(guess=iter(guesses,"row"), 
+                 .packages='pomp', 
+                 .combine=rbind,
+               .options.multicore=list(set.seed=TRUE),
+               .export=c("flu","fixed_params")
+               ) %dopar% 
+       {
+         mf <- mif2(flu, start=unlist(guess), Np=2000, Nmif=50,
+                    cooling.type="geometric",
+                    cooling.fraction.50=0.5,
+                    transform=TRUE,
+                    rw.sd=rw.sd(Beta=0.02,mu_I=0.02,rho=0.02)
+                    )
+         mf <- mif2(mf,Nmif=100)
+         ll <- replicate(10,logLik(pfilter(mf,Np=100000)))
+         ll <- logmeanexp(ll,se=TRUE)
+         c(coef(mf),loglik=ll[1],loglik=ll[2])
+       }
+     }) -> results_global
+results_global <- as.data.frame(results_global)
+results <- rbind(results,results_global[names(results)])
+write.csv(results,file="seir3_params.csv",row.names=FALSE)
+
+#' 
+## ------------------------------------------------------------------------
+pairs(~loglik+Beta+mu_E+mu_I+rho, data=subset(results,loglik>max(loglik)-10),pch=16)
 
 #' 
 #' ----------------------------
