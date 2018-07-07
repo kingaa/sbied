664c271c2a9732d941eae2f1138bbfe0a6cea50a
Aaron A. King <kingaa@umich.edu>
2018-07-07 10:22:41 -0400

    update

---
+++ admin/spam.R
@@ -0,0 +1,27 @@
+library(aakmisc)
+library(magrittr)
+library(plyr)
+library(readr)
+library(stringi)
+
+batchsize <- 60
+replyto <- "kingaa.sismid@gmail.com"
+subject <- "SISMID Module 7 Advance Instructions"
+cmd <- 'REPLYTO=%s mutt -s "%s" -b %s %s < msg.txt\n'
+
+read_csv("list.csv") %>%
+  mutate(
+    batch=1+seq_along(email)%/%batchsize,
+    firstname=stri_replace_first_regex(registrant,"(\\w+),\\s+(\\w+)","$2"),
+    lastname=stri_replace_first_regex(registrant,"(\\w+),\\s+\\w+","$1")
+    ) -> addresses
+
+daply(
+      addresses,
+      ~batch,
+      function (x) {
+        sprintf(cmd,replyto,subject,paste(x$email,collapse=","),replyto)
+      }
+      ) -> cmds
+
+cat(cmds,file="spam.sh")
