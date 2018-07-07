14e7c7888488d769f73fc2ecef9171c2f62f2133
Aaron A. King <kingaa@umich.edu>
2018-07-07 10:52:57 -0400

    tweak

--- prep/spam.R
+++ prep/spam.R
@@ -6,20 +6,24 @@ library(stringi)
 batchsize <- 60
 replyto <- "kingaa.sismid@gmail.com"
 subject <- "SISMID Module 7 Advance Instructions"
-cmd <- 'REPLYTO=%s mutt -s "%s" -b %s %s < msg.txt\n'
+cmd <- 'REPLYTO=%s mutt -s "%s" -b %s -- %s < msg.txt\n'
 
 read_csv("list.csv",col_types="cc") %>%
   mutate(
     batch=1+seq_along(email)%/%batchsize,
-    firstname=stri_replace_first_regex(registrant,"(\\w+),\\s+(\\w+)","$2"),
-    lastname=stri_replace_first_regex(registrant,"(\\w+),\\s+\\w+","$1")
+    firstname=stri_replace_first_regex(registrant,"(\\w+),\\s*(\\w+)","$2"),
+    lastname=stri_replace_first_regex(registrant,"(\\w+),\\s*(\\w+)","$1")
     ) -> addresses
 
 daply(
       addresses,
       ~batch,
       function (x) {
-        sprintf(cmd,replyto,subject,paste(x$email,collapse=","),replyto)
+        sprintf(cmd,
+                replyto=replyto,
+                subj=subject,
+                bcc=paste(x$email,collapse=","),
+                to=replyto)
       }
       ) -> cmds
 
