c9466f88c78a6d94fdeba6d5d18af5d58169aa10
Aaron A. King <kingaa@umich.edu>
2020-07-10 14:23:28 -0400

    Revert "fix typos"
    This reverts commit a49bc0fb4cc8d76fe4234096955e377ced08f5f9.

--- measles/main.R
+++ measles/main.R
@@ -261,7 +261,6 @@ simdat %>%
          data=plyr::mapvalues(data,from=c(TRUE,FALSE),to=c("data","simulation"))) %>%
   spread(p,q) %>%
   ggplot(aes(x=time,y=med,color=data,fill=data,ymin=lo,ymax=hi))+
-  scale_color_manual(values=c(`TRUE`="blue",`FALSE`="red"))+
   geom_ribbon(alpha=0.2)+
   guides(data=FALSE)+
   labs(y="cases") -> pl1
@@ -271,7 +270,6 @@ simdat %>%
   mutate(data=.id=="data") %>%
   ggplot(aes(x=time,y=cases,group=.id,color=data))+
   geom_line()+
-  scale_color_manual(values=c(`TRUE`="blue",`FALSE`="red"))+
   guides(color=FALSE) -> pl2
 
 library(cowplot)
