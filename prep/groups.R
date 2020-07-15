bb2805b670082da27ea76eab7b8d29654a44b8d5
Aaron A. King <kingaa@umich.edu>
2020-07-15 12:37:53 -0400

    update

--- prep/groups.R
+++ prep/groups.R
@@ -1,21 +1,30 @@
 library(tidyverse)
+library(stringi)
 
 read_csv("list.csv") %>%
   mutate(
+    first=stri_replace_first_regex(Registrant,"^(.+?),\\s*(.+?)$","$2") %>% stri_trans_totitle(),
+    last=stri_replace_first_regex(Registrant,"^(.+?),\\s*(.+?)$","$1") %>% stri_trans_totitle(),
+    slack=sprintf("@%s %s",first,last),
     rank=rank(runif(n=n()))-1,
     ngroup=ceiling(n()/5),
     gp=(rank%%ngroup)+1,
     group=sprintf("room%02d",gp)
   ) %>%
-  arrange(group,registrant) %>%
+  arrange(Registrant) %>%
   select(
-    `Pre-assign Room Name`=group,
-    `Email Address`=email,
-    Name=registrant
+    Name=Registrant,
+    Email="E-mail",
+    Slack=slack,
+    Group=group,
+    Timezone
   ) -> dat
 
 dat %>%
-  select(-Name) %>%
+  select(
+    `Pre-assign Room Name`=Group,
+    `Email Address`=Email
+  ) %>%
   write_csv("zoom_rooms.csv")
 
 dat %>%
