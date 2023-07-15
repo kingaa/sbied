f0afb1852735b378f8aed5bc10cdd0e0098dace7
Aaron A. King <kingaa@umich.edu>
2023-07-15 13:08:35 -0400

    rebuild contacts

--- contacts/main.R
+++ contacts/main.R
@@ -1,20 +1,12 @@
-DEBUG <- FALSE
-# DEBUG <- TRUE
 library(doFuture)
-library(doRNG)
- plan(multisession)
-registerDoRNG(2050320976)
-cores <- nbrOfWorkers()
 
 
 
 par(mai=c(0.8,0.8,0.1,0.1))
 contact_data <- read.table(file="contacts.csv",header=TRUE)
 matplot(t(contact_data[1:15,1:4]),
-        ylab="total sexual contacts",xlab="6-month intervals", 
-        type="l",xaxp=c(1,4,3))
-
-
+  ylab="total sexual contacts",xlab="6-month intervals",
+  type="l",xaxp=c(1,4,3))
 
 library(panelPomp)
 contacts <- contacts()
@@ -27,17 +19,22 @@ coef(contacts)
 
 
 
-stew("pfilter1.rda",{
-  tic <- Sys.time()
-  eval_cores <- cores
-  pf1_results <- foreach(i=1:20) %dofuture% {
-    library(panelPomp)
-    pf <- pfilter(contacts,Np= if(DEBUG) 10 else 2000)
-    list(logLik=logLik(pf),
-         unitLogLik=sapply(unitobjects(pf),logLik))
+bake("pfilter1.rds",{
+  plan(multicore,workers=20)
+  pf1_results <- foreach(
+    i=1:20,.options.future=list(seed=TRUE)
+  ) %dofuture% {
+    pf <- pfilter(contacts,Np=2000)
+    list(
+      logLik=logLik(pf),
+      unitLogLik=sapply(unitobjects(pf),logLik)
+    )
   }
-  t1 <- as.numeric(difftime(Sys.time(),tic,units="mins"))
-}) 
+  attr(pf1_results,"ncpu") <- nbrOfWorkers()
+  pf1_results
+}) -> pf1_results
+t1 <- attr(pf1_results,"system.time")[3]/60
+eval_cores <- attr(pf1_results,"ncpu")
 
 loglik1 <- sapply(pf1_results,function(x) x$logLik)
 logmeanexp(loglik1,se=T)
@@ -47,37 +44,42 @@ panel_logmeanexp(pf1_loglik_matrix,MARGIN=1,se=T)
 
 
 
-stew("mif1.rda",{
-  mif_cores <- cores
-  tic <- Sys.time()
-mif_results <- foreach(i=1:20) %dofuture% {
-  library(pomp); library(panelPomp)
-  mf <- mif2(contacts,
-    Nmif = if(DEBUG) 2 else 50,
-    Np = if(DEBUG) 5 else 1000,
-    cooling.type="geometric", # needed for panelPomp 0.10
-    cooling.fraction.50=0.5,
-    rw.sd=rw_sd(mu_X=0.02, sigma_X=0.02,mu_D = 0.02,
-      sigma_D=0.02,mu_R=0.02, alpha=0.02)
-  )
-  list(logLik=logLik(mf),params=coef(mf))
-}
-  t2 <- difftime(Sys.time(),tic,units="mins")
-})
+bake("mif1.rds",{
+  plan(multicore,workers=20)
+  mif_results <- foreach(
+    i=1:20,
+    .options.future=list(seed=TRUE)
+  ) %dofuture% {
+    mf <- mif2(contacts,
+      Nmif=50, Np=1000,
+      cooling.type="geometric", # note difference with pomp
+      cooling.fraction.50=0.5,
+      rw.sd=rw_sd(mu_X=0.02, sigma_X=0.02,mu_D = 0.02,
+        sigma_D=0.02,mu_R=0.02, alpha=0.02)
+    )
+    list(logLik=logLik(mf),params=coef(mf))
+  }
+  attr(mif_results,"ncpu") <- nbrOfWorkers()
+  mif_results
+}) -> mif_results
+t2 <- attr(mif_results,"system.time")[3]/60
+mif_cores <- attr(mif_results,"ncpu")
 
 
 
-stew("mif1-lik-eval.rda",{
-tic <- Sys.time()
-mif_logLik <-  sapply(mif_results,function(x)x$logLik)
-mif_mle <- mif_results[[which.max(mif_logLik)]]$params
-pf3_loglik_matrix <- foreach(i=1:10,.combine=rbind) %dofuture% {
-  library(panelPomp)
-  unitlogLik(pfilter(contacts,
-    shared=mif_mle,Np=if(DEBUG) 50 else 10000))
-}  
-t3 <- difftime(Sys.time(),tic,units="mins")
-mif_lik_eval_cores <- cores
-})
+bake("mif1-lik-eval.rds",{
+  mif_logLik <-  sapply(mif_results,function(x)x$logLik)
+  mif_mle <- mif_results[[which.max(mif_logLik)]]$params
+  plan(multicore,workers=10)
+  pf3_loglik_matrix <- foreach(i=1:10,.combine=rbind,
+    .options.future=list(seed=TRUE)
+  ) %dofuture% {
+    unitlogLik(pfilter(contacts,shared=mif_mle,Np=10000))
+  }
+  attr(pf3_loglik_matrix,"ncpu") <- nbrOfWorkers()
+  pf3_loglik_matrix
+}) -> pf3_loglik_matrix
+t3 <- attr(pf3_loglik_matrix,"system.time")[3]/60
+mif_lik_eval_cores <- attr(pf3_loglik_matrix,"ncpu")
 
 panel_logmeanexp(pf3_loglik_matrix,MARGIN=2,se=T)
